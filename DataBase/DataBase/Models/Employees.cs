16195124dde28dc3794a01a44a6f7feb4845a187
N-Banchik <87769833+N-Banchik@users.noreply.github.com>
2021-11-15 14:45:55 +0200

    creating DB models

---
+++ DataBase/DataBase/Models/Employees.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataBase.Models
+{
+    [Table("Employees_TB")]
+    class Employees
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        [Column("First Name")]
+        [DataType("Nvarchar")]
+        [MaxLength(25)]
+        public string First_Name{ get; set; }
+        [Required]
+        [Column("Last Name")]
+        [DataType("Nvarchar")]
+        [MaxLength(25)]
+        public string last_Name { get; set; }
+        [Required]
+        [Column("BirthDate")]
+        [DataType("smalldatetime")]
+        public DateTime Birthdate{ get; set; }
+        public int MyProperty { get; set; }
+
+        [Required]
+        [Column("Manager")]
+        [DataType("int")]
+        public int Is_Manager{ get; set; }
+
+    }
+}
