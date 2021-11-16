de57ecf70b501b521900a0efff0fa6b0002ea9e0
N-Banchik <87769833+N-Banchik@users.noreply.github.com>
2021-11-16 12:38:02 +0200

    employees dbset -work costumer dbset- work

--- DataBase/DataBase/Models/Costumers.cs
+++ DataBase/DataBase/Models/Costumers.cs
@@ -5,10 +5,38 @@ using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
 
 namespace DataBase.Models
 {
+    
     class Costumers
     {
+        [Key]
+        [Column("ID")]
+        [DataMember]
+        public int Costumer_ID { get; set; }
+
+        [Required]
+        [Column("First Name")]
+        [DataType("nvarchar")]
+        [MaxLength(25)]
+        public string First_Name { get; set; }
+        [Required]
+        [Column("Last Name")]
+        [DataType("nvarchar")]
+        [MaxLength(25)]
+        public string last_Name { get; set; }
+        [Required]
+        [Column("BirthDate")]
+        [DataType("smalldatetime")]
+        public DateTime Birthdate { get; set; }
+
+        [Required]
+        [Column("Address")]
+        [DataType("nvarchar")]
+        [MaxLength(50)]
+        public string Address { get; set; }
+
     }
 }
--- DataBase/DataBase/Models/Employees.cs
+++ DataBase/DataBase/Models/Employees.cs
@@ -16,24 +16,41 @@ namespace DataBase.Models
 
         [Required]
         [Column("First Name")]
-        [DataType("Nvarchar")]
+        [DataType("nvarchar")]
         [MaxLength(25)]
         public string First_Name{ get; set; }
         [Required]
         [Column("Last Name")]
-        [DataType("Nvarchar")]
+        [DataType("nvarchar")]
         [MaxLength(25)]
         public string last_Name { get; set; }
         [Required]
         [Column("BirthDate")]
         [DataType("smalldatetime")]
         public DateTime Birthdate{ get; set; }
-        public int MyProperty { get; set; }
+        [Required]
+        [Column("Hire Date")]
+        [DataType("smalldatetime")]
+        public DateTime Hire_Date { get; set; }
+        [Required]
+        [Column("Address")]
+        [DataType("nvarchar")]
+        [MaxLength(50)]
+        public string Address{ get; set; }
+        [Required]
+        [Column("Phone number")]
+        [DataType("nvarchar")]
+        [MaxLength(15)]
+        public string Phone { get; set; }
 
         [Required]
         [Column("Manager")]
         [DataType("int")]
         public int Is_Manager{ get; set; }
+
+        [NotMapped]
+        bool Manager;
+
 
     }
 }
