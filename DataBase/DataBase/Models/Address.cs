e17968a37b28d054b2d7005874ad1d991aa30e38
N-Banchik <87769833+N-Banchik@users.noreply.github.com>
2021-11-16 13:37:05 +0200

    add addresses dbset

---
+++ DataBase/DataBase/Models/Address.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBase.Models
+{
+    class Address
+    {
+        public int User_ID { get; set; }
+        public string Street_Name { get; set; }
+        public int House_Number { get; set; }
+
+        public int Apartment { get; set; }
+    }
+}
